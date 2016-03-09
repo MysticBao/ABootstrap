dead92d97d00a7521ca269cece9ed698e3eb5fc6
Allen Yin <allen.yin@sap.com>
2016-03-09 17:21:31 +0800

    modify env js

--- js/envTable.js
+++ js/envTable.js
@@ -7,48 +7,24 @@
 function showzhihuTable()
 		{
 			document.getElementById("tbltugua").style.display="none";
-			document.getElementById("tblPreview").style.display="none";
 			document.getElementById("tblCand").style.display="none";
-			document.getElementById("tblRot").style.display="none";
 			document.getElementById("tblzhihu").style.display="";
-		}
-		function showPreviewTable()
-		{
-			document.getElementById("tbltugua").style.display="none";
-			document.getElementById("tblzhihu").style.display="none";
-			document.getElementById("tblCand").style.display="none";
-			document.getElementById("tblRot").style.display="none";
-			document.getElementById("tblPreview").style.display="";
 		}
 		function showCandTable()
 		{
 			document.getElementById("tbltugua").style.display="none";
 			document.getElementById("tblzhihu").style.display="none";
-			document.getElementById("tblPreview").style.display="none";
-			document.getElementById("tblRot").style.display="none";
 			document.getElementById("tblCand").style.display="";
-		}
-		function showRotTable()
-		{
-			document.getElementById("tbltugua").style.display="none";
-			document.getElementById("tblzhihu").style.display="none";
-			document.getElementById("tblPreview").style.display="none";
-			document.getElementById("tblCand").style.display="none";
-			document.getElementById("tblRot").style.display="";
 		}
 		function showtuguaTable()
 		{
 			document.getElementById("tblzhihu").style.display="none";
-			document.getElementById("tblPreview").style.display="none";
 			document.getElementById("tblCand").style.display="none";
-			document.getElementById("tblRot").style.display="none";
 			document.getElementById("tbltugua").style.display="";
 		}
 		function removeAllTable()
 		{
 			document.getElementById("tbltugua").style.display="none";
 			document.getElementById("tblzhihu").style.display="none";
-			document.getElementById("tblPreview").style.display="none";
 			document.getElementById("tblCand").style.display="none";
-			document.getElementById("tblRot").style.display="none";
 		}
